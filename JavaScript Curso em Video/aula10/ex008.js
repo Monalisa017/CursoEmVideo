8e2c1a638fcc68579816cb020f5259c0192d4d87
Monalisa <monalisa.silva@aluno.faculdadeimpacta.com.br>
2021-05-19 18:08:08 -0300

    Inclusão de exercicios 8,9 e 10. Desen. des 1

---
+++ JavaScript Curso em Video/aula10/ex008.js
@@ -0,0 +1,9 @@
+console.log('O console funcionou corretamente! ')
+
+var velocidade = 50.5
+
+console.log(`A velocidade do seu carro é equivalente à ${velocidade} Km/h`)
+if(velocidade > 60){
+    console.log(`O limite de velocidade é 60 Km/h, você estava a ${velocidade} Km/h e por isso foi MULTADO! `)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)

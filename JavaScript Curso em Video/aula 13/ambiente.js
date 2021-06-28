3725e952399c1fee42c5caee665ffb1e01decd1d
Monalisa017 <61711279+Monalisa017@users.noreply.github.com>
2021-06-28 12:57:35 -0300

    Inclusão dos exercicios estrutura if

---
+++ JavaScript Curso em Video/aula 13/ambiente.js
@@ -0,0 +1,16 @@
+// Teste lógico no início
+//var c = 1
+// while (c <= 10){
+//     console.log(`Passo ${c}`)
+//     c ++
+    
+// }
+// Teste lógico no final
+ var c = 1
+ do {
+        console.log(`Passo ${c}`)
+        c ++
+    }while(c <= 10)
+
+    
+
